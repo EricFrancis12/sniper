d7bdf6b84d172ac04f6b255b93d64d58f11f5902
Eric <ericfrancis1@proton.me>
2025-01-18 16:27:37 -0600

    remove background.ts & app disabled state

--- src/contentScript.isolated.tsx
+++ src/contentScript.isolated.tsx
@@ -1,19 +1,21 @@
-import { sniperDataSchema } from "./lib/schemas";
-import { SniperData } from "./lib/types";
-
 console.log("~ isolated 1");
 
 window.addEventListener("load", () => {
     console.log("~ isolated 2");
 
-    chrome.action.setBadgeText({ text: "456" });
-
     chrome.storage.sync.get(
-        { campaigns: [] } satisfies SniperData,
-        async (items) => {
-            const { data, success } = await sniperDataSchema.spa(items);
-            if (success) {
-                window.postMessage(data, "*");
+        {
+            campaigns: [],
+            disabled: false,
+        },
+        (items) => {
+            console.log(items);
+            if ("campaigns" in items) {
+                window.postMessage({ campaigns: items.campaigns }, "*");
+            }
+            if ("disabled" in items && (items.disabled === "true" || items.disabled === "false")) {
+                console.log("here");
+                chrome.runtime.sendMessage(items.disabled);
             }
         },
     );
